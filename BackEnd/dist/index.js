e5d30209c0b79abccda29066c548e90c2c455061
AN-Pradip <ppradip.an@gmail.com>
2023-04-15 18:56:48 +0200

    Creating connection to database
    creating connection to database using environment variables

--- BackEnd/dist/index.js
+++ BackEnd/dist/index.js
@@ -4,10 +4,59 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const mysql_1 = __importDefault(require("mysql"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const app = (0, express_1.default)();
+app.use(express_1.default.urlencoded({ extended: false }));
+app.use(express_1.default.json());
 app.get('/', (req, res) => {
     res.send('running on base path');
 });
-app.listen(3000, () => {
-    console.log('listening on port 3000');
+app.get('/login', (req, res) => {
+    var pool = mysql_1.default.createPool({
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE,
+        port: parseInt(process.env.DATABASE_PORT || "", 10),
+        connectionLimit: 10,
+        multipleStatements: true
+    });
+    pool.getConnection(function (err, conn) {
+        //if connection error
+        if (err) {
+            console.log(err);
+            res.send({
+                success: false,
+                statusCode: 500
+            });
+            return;
+        }
+        //find User table
+        conn.query('SELECT * from User', function (err, table) {
+            //if error in finding user table
+            if (err) {
+                conn.release();
+                return res.send({
+                    success: false,
+                    statusCode: 400,
+                });
+            }
+            //show table
+            res.send({
+                statusCode: 200,
+                data: table
+            });
+            conn.release();
+        });
+    });
+});
+app.post('/test', (req, res) => {
+    res.send({
+        data: req.body
+    });
+});
+app.listen(process.env.LOCAL_PORT, () => {
+    console.log(`listening on port ${process.env.LOCAL_PORT} `);
 });
